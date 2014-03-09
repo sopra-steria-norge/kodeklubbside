7f27749207f7d1ade9833a226ad9f7bb3737d0fc
Trude <trude.martinsen@gmail.com>
2014-03-09 14:54:16 +0100

    Lagt til genererte kodeklubbfiler for termin 3

--- hello.py
+++ hello.py
@@ -5,6 +5,4 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def hello():
-    #return 'Hello World!'
-    #return app.send_static_file('index.html')
-    return redirect(url_for('static', filename='filen.html'))
+    return redirect(url_for('static', filename='index.html'))
