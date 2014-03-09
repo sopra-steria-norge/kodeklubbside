96fda58810ce713fdcaf058044efe6b70892d8a1
Trude <trude.martinsen@gmail.com>
2014-03-09 14:48:49 +0100

    Serving a htmlfile

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask, request, url_for, redirect
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    #return 'Hello World!'
+    #return app.send_static_file('index.html')
+    return redirect(url_for('static', filename='filen.html'))
